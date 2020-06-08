1337860ada8e3f43cc2d4b355c4708b506a6afbd
SeolRoh <seven800@naver.com>
2020-06-08 14:12:25 +0900

    config_download_username_session

--- src/main/java/com/companyd/hompage/seoul/entity/Users.java
+++ src/main/java/com/companyd/hompage/seoul/entity/Users.java
@@ -15,4 +15,60 @@ public class Users {
     private String phoneNumber;
     private int isMember;
     private int admin;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getIsMember() {
+        return isMember;
+    }
+
+    public void setIsMember(int isMember) {
+        this.isMember = isMember;
+    }
+
+    public int getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(int admin) {
+        this.admin = admin;
+    }
 }
