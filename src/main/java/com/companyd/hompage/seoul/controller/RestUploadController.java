dc68fa626c51630e41630c81ca2d0ee034955e5c
griffin <griffindouble@gmail.com>
2020-06-10 15:43:18 +0900

    change MongoDB update service location

--- src/main/java/com/companyd/hompage/seoul/controller/RestUploadController.java
+++ src/main/java/com/companyd/hompage/seoul/controller/RestUploadController.java
@@ -122,49 +122,6 @@ public class RestUploadController {
         return new ResponseEntity("Successfully uploaded!", HttpStatus.OK);
     }
 
-    // SummaryData update 쿼리
-    @PostMapping("/updateSummaryData/{fileName}")
-    public ModelAndView updateSummaryData(@PathVariable String fileName, HttpServletRequest request){
-        System.out.println(fileName);
-        Enumeration<String> em = request.getParameterNames();
-        List<Summary> summaryList = new LinkedList<Summary>();
-        Summary newSummary;
-        int i = 0;
-        String dataTypeValue = "";
-        String deIdentifiedValue = "";
-        String proveValue = "";
-        while (em.hasMoreElements()) {
-            String name = em.nextElement();
-            int pos = name.lastIndexOf("_") + 1; // dataType_1 ->
-            String idx = name.substring(pos); // idx -> 1
-            if(i % 3 == 0){
-                dataTypeValue = request.getParameter("dataType_" + idx);
-                // System.out.println("dataTypeValue : " + dataTypeValue);
-            }else if(i%3 == 1){
-                deIdentifiedValue = request.getParameter("deIdentified_" + idx);
-                // System.out.println("deIdentifiedValue : " + deIdentifiedValue);
-            }else{
-                proveValue = request.getParameter("prove_" + idx);
-                // System.out.println("proveValue : " + proveValue);
-                newSummary = new Summary();
-                newSummary.setProve(proveValue);
-                newSummary.setDataType(dataTypeValue);
-                newSummary.setDeIdentified(deIdentifiedValue);
-                summaryList.add(newSummary);
-            }
-            i++;
-        }
-        // System.out.println("end!");
-        SummaryData summaryData = summaryService.getSummaryByFileName(fileName);
-        List<Info> infos = summaryData.getInfo();
-        for(int num = 0; num<infos.size(); num++){
-            infos.get(num).setSummary(summaryList.get(num));
-        }
-        summaryData.setInfo(infos);
-        summaryService.updateSummaryData(summaryData);
-        ModelAndView mav = new ModelAndView("mypageFilelist");
-        // model and view 만들어서 리턴
-        return mav;
-    }
+
 
 }
